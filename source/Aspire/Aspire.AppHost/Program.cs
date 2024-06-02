210eea2c53df2f49167ed60228d5ef81630bf605
Vladimir Matos <vladimir.matos@outlook.com>
2024-06-02 13:37:38 -0300

    feat: add rabbitmq aspire projects references

--- source/Aspire/Aspire.AppHost/Program.cs
+++ source/Aspire/Aspire.AppHost/Program.cs
@@ -1,9 +1,16 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
+var rabbitMQ = builder.AddRabbitMQ("rabbitMQ")
+                      .WithEnvironment("RABBITMQ_DEFAULT_USER", "admin")
+                      .WithEnvironment("RABBITMQ_DEFAULT_PASS", "admin")
+                      .WithManagementPlugin();
+
 builder.AddProject<Projects.Sensor_Manager>("sensor-manager");
 
-builder.AddProject<Projects.Data_Collector>("data-collector");
+builder.AddProject<Projects.Data_Collector>("data-collector")
+       .WithReference(rabbitMQ);
 
-builder.AddProject<Projects.Calibrator_Analyzer>("calibrator-analyzer");
+builder.AddProject<Projects.Calibrator_Analyzer>("calibrator-analyzer")
+       .WithReference(rabbitMQ);
 
 builder.Build().Run();
